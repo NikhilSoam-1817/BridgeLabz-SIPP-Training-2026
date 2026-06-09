23eca73fcbd03c48f40af8240a2a38a499ed2f6b
Nikihl_Soam <soamnikihl64@gmail.com>
2026-06-09 22:32:03 +0530

    [nikhil soam]

---
+++ core-java-practice/gcr-codebase/proramming-element/UserDiscount.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class UserDiscount {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter fee: ");
+        double fee = input.nextDouble();
+
+        System.out.print("Enter discount percent: ");
+        double discountPercent = input.nextDouble();
+
+        double discount =
+                (fee * discountPercent) / 100;
+
+        double finalFee = fee - discount;
+
+        System.out.println(
+                "The discount amount is INR " +
+                discount +
+                " and final discounted fee is INR " +
+                finalFee);
+
+        input.close();
+    }
+}
